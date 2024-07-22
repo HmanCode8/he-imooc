b9d95ff2a38665a0e761627b7fde18db05fc0a29
278073309@qq.com <278073309@qq.com>
2024-07-22 12:31:38 +0800

    feat:盐城大屏:运维管理界面布局

--- src/components/panels/RightOperationMaintenance.vue
+++ src/components/panels/RightOperationMaintenance.vue
@@ -1,61 +1,74 @@
 <script setup lang="ts">
-import { useEffct } from 'vue'
-import FristLevelTitle from '../common/FirstLevelTitle.vue'
-import SecondLevelTitle from '../common/SecondLevelTitle.vue'
-import ThirdLevelTitle from '../common/ThirdLevelTitle.vue'
-import PipeChart from '../charts/pipeChart.vue'
-import BarRowChartTow from '../charts/barRowChartTow.vue'
-import pipeChart from '../charts/pipeChart.vue'
+import { ref,useEffct } from "vue";
+import FristLevelTitle from "../common/FirstLevelTitle.vue";
+import SecondLevelTitle from "../common/SecondLevelTitle.vue";
+import ThirdLevelTitle from "../common/ThirdLevelTitle.vue";
+import PipeChart from "../charts/pipeChart.vue";
+import BarRowChartTow from "../charts/barRowChartTow.vue";
+import pipeChart from "../charts/pipeChart.vue";
 
 import { useGlobalStore } from "@/store";
 
-const global = useGlobalStore()
+const global = useGlobalStore();
 
 const changeStore = () => {
-    console.log(global.navActive)
-    global.setNavActive('/dashboard')
-}
+  console.log(global.navActive);
+  global.setNavActive("/dashboard");
+};
 const arr1 = [
-    {
-        name: '示范区',
-        value: 1000
-    },
-    {
-        name: '建成区',
-        value: 100
-    }
-]
+  {
+    name: "示范区",
+    value: 1000
+  },
+  {
+    name: "建成区",
+    value: 100
+  }
+];
 
-
+const pipeChartdata = ref([
+  { name: "燃气", percentage: 24, distance: "25km", color: "#FF6384" },
+  { name: "盐都区", percentage: 24, distance: "25km", color: "#FFCE56" },
+  { name: "大丰区", percentage: 24, distance: "15km", color: "#36A2EB" },
+  { name: "建湖县", percentage: 24, distance: "25km", color: "#FFA07A" },
+  { name: "阜宁县", percentage: 24, distance: "25km", color: "#4BC0C0" },
+  { name: "滨海县", percentage: 24, distance: "25km", color: "#FF6384" },
+  { name: "响水县", percentage: 24, distance: "25km", color: "#FFCE56" },
+  { name: "东台市", percentage: 24, distance: "45km", color: "#36A2EB" },
+  { name: "射阳县", percentage: 24, distance: "25km", color: "#FFA07A" }
+]);
 </script>
 
 <template>
-    <div class="overview">
-        <FristLevelTitle title="综合监控"></FristLevelTitle>
-        <SecondLevelTitle title="基础概况21"></SecondLevelTitle>
-        <div class="overview-content">
-            <div class="l w-1/3">
-                <ul class="flex flex-col">
-                    <li class="flex" v-for="item in arr1" :key="item.name">
-                        <div class="icon w-10 h-10 bg-slate-500 rounded-full"></div>
-                        <div class="flex">
-                            <div>{{ item.name }}</div>
-                            <div class="value">{{ item.value }}</div>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div class="overview-content w-full h-full flex flex-wrap">
-            <div class="w-1/2 h-1/4 ">
-                <BarRowChartTow />
-            </div>
-            <div class="w-1/2 h-1/4 ">
-                <pipeChart />
+  <div class="pipe-analy">
+    <FristLevelTitle title="安全专项检查"></FristLevelTitle>
+    <div class="flex w-full h-1/3  flex-wrap justify-between">
+      <div class="8k:w-1/2 4k:w-full">
+        <ThirdLevelTitle class="w-full" title="检查次数"></ThirdLevelTitle>
+        <Pipe3dChartTwo class="w-full h-full flex" :legend="pipeChartdata" />
+      </div>
 
-            </div>
-        </div>
+      <div class="8k:w-1/2 4k:w-full ">
+        <ThirdLevelTitle title="上报问题数量"></ThirdLevelTitle>
+        <Bar3dChart />
+      </div>
     </div>
+    <div class="flex w-full h-1/3  flex-wrap justify-between">
+      <ThirdLevelTitle title="整改已完成/未完成数量"></ThirdLevelTitle>
+      <Bar3dChart />
+    </div>
+
+    <FristLevelTitle title="维护维修  次数/频率"></FristLevelTitle>
+    <div class="flex w-full h-1/3 flex-wrap justify-between">
+      <div class="8k:w-1/2 4k:w-full">
+        <Pipe3dChartTwo class="w-full h-full flex" :legend="pipeChartdata" />
+      </div>
+
+      <div class="8k:w-1/2 4k:w-full ">
+        <Bar3dChart />
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss"></style>
